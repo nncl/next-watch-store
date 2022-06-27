cc9e0dd2a06d97327ce9e97a4ab13973b8e1a5fb
Cauê Almeida <nncl@live.com>
2022-06-27 20:00:28 -0300

    test(components): Testing search submitting with value

--- components/search.spec.js
+++ components/search.spec.js
@@ -1,5 +1,6 @@
 import Search from './search';
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const doSearch = jest.fn();
 
@@ -12,12 +13,30 @@ describe('Search', () => {
     expect(screen.getByRole('form')).toBeInTheDocument();
   });
 
-  fit('should call props.doSearch() when form is submitted', async () => {
+  it('should render a input type equals search', () => {
+    render(<Search doSearch={doSearch} />);
+    expect(screen.getByRole('searchbox')).toBeInTheDocument();
+  });
+
+  it('should call props.doSearch() when form is submitted', async () => {
     render(<Search doSearch={doSearch} />);
 
     const form = screen.getByRole('form');
     await fireEvent.submit(form);
 
     expect(doSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call props.doSearch() with the user input', async () => {
+    render(<Search doSearch={doSearch} />);
+
+    const inputText = 'Some search';
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, inputText);
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenLastCalledWith(inputText);
   });
 });
