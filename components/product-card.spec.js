fd0fd1d20c88e1a4389da2188b21775220e6d6ee
Cauê Almeida <nncl@live.com>
2022-06-28 22:17:43 -0300

    test(components): Testing product card button click

--- components/product-card.spec.js
+++ components/product-card.spec.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import ProductCard from './product-card';
 
 const product = {
@@ -8,20 +8,36 @@ const product = {
     'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
 };
 
+const addToCart = jest.fn();
+
+const renderProduct = () => {
+  render(<ProductCard product={product} addToCart={addToCart} />);
+};
+
 describe('ProductCard', () => {
   it('should render ProductCard', () => {
-    render(<ProductCard product={product} />);
+    renderProduct();
 
     expect(screen.getByTestId('product-card')).toBeInTheDocument();
   });
 
   it('should display proper data', () => {
-    render(<ProductCard product={product} />);
+    renderProduct();
 
     expect(screen.getByText(new RegExp(product.title, 'i'))).toBeInTheDocument();
     expect(screen.getByText(new RegExp(product.price, 'i'))).toBeInTheDocument();
     expect(screen.getByTestId('image')).toHaveStyle({
       backgroundImage: product.image,
     });
+  });
+
+  it('should call props.addToCart() when button gets clicked', async () => {
+    renderProduct();
+
+    const button = screen.getByRole('button');
+    await fireEvent.click(button);
+
+    expect(addToCart).toHaveBeenCalledTimes(1);
+    expect(addToCart).toHaveBeenCalledWith(product);
   });
 });
