18b02c3dc94ddd0f9caedf04d19028ec2a8017fd
Cauê Almeida <nncl@live.com>
2022-07-05 20:40:38 -0300

    test(components): Testing cart-item quantity binding

--- components/cart-item.js
+++ components/cart-item.js
@@ -1,6 +1,14 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function CartItem({ product }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const increase = () => setQuantity(quantity + 1);
+  const decrease = () => {
+    setQuantity(quantity > 0 ? quantity - 1 : 0);
+  };
+
   return (
     <div data-testid="cart-item" className="flex justify-between mt-6">
       <div className="flex">
@@ -17,7 +25,10 @@ export default function CartItem({ product }) {
         <div className="mx-3">
           <h3 className="text-sm text-gray-600">{product.title}</h3>
           <div className="flex items-center mt-2">
-            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+            <button
+              onClick={() => decrease()}
+              className="text-gray-500 focus:outline-none focus:text-gray-600"
+            >
               <svg
                 className="h-5 w-5"
                 fill="none"
@@ -30,8 +41,13 @@ export default function CartItem({ product }) {
                 <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
-            <span className="text-gray-700 mx-2">2</span>
-            <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+            <span data-testid="quantity" className="text-gray-700 mx-2">
+              {quantity}
+            </span>
+            <button
+              onClick={() => increase()}
+              className="text-gray-500 focus:outline-none focus:text-gray-600"
+            >
               <svg
                 className="h-5 w-5"
                 fill="none"
--- components/cart-item.spec.js
+++ components/cart-item.spec.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import CartItem from './cart-item';
 
 const product = {
@@ -30,5 +30,45 @@ describe('CartItem', () => {
     expect(screen.getByText(new RegExp(product.price, 'i'))).toBeInTheDocument();
     expect(image).toHaveProperty('src');
     expect(image).toHaveProperty('alt', product.title);
+  });
+
+  it('should display 1 as initial quantity', () => {
+    renderCartItem();
+
+    expect(screen.getByTestId('quantity').textContent).toBe('1');
+  });
+
+  it('should increase quantity by 1 when second button is pressed', () => {
+    renderCartItem();
+
+    const [_, button] = screen.getAllByRole('button');
+
+    fireEvent.click(button);
+
+    expect(screen.getByTestId('quantity').textContent).toBe('2');
+  });
+
+  it('should decrease quantity by 1 when first button is pressed', () => {
+    renderCartItem();
+
+    const [buttonDecrease, buttonIncrease] = screen.getAllByRole('button');
+    const quantity = screen.getByTestId('quantity');
+
+    fireEvent.click(buttonIncrease);
+    expect(quantity.textContent).toBe('2');
+
+    fireEvent.click(buttonDecrease);
+    expect(quantity.textContent).toBe('1');
+  });
+
+  it('should not go below zero in the quantity', () => {
+    renderCartItem();
+
+    const [buttonDecrease] = screen.getAllByRole('button');
+    const quantity = screen.getByTestId('quantity');
+
+    fireEvent.click(buttonDecrease);
+    fireEvent.click(buttonDecrease);
+    expect(quantity.textContent).toBe('0');
   });
 });
