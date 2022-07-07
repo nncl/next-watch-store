74b952126e83bf52baab52fa72da56b5f1393b61
Cauê Almeida <nncl@live.com>
2022-07-07 18:11:16 -0300

    test(pages): Adding TODOs

--- page-tests/product-list.integration.spec.js
+++ page-tests/product-list.integration.spec.js
@@ -7,4 +7,12 @@ describe('ProductList', () => {
 
     expect(screen.getByTestId('product-list')).toBeInTheDocument();
   });
+
+  it.todo('should render the ProductCard component 10 times');
+  it.todo('should render the no products message');
+  it.todo('should render the Search component');
+  it.todo('should filter the product list when a search is performed');
+  it.todo('should display error message when promise rejects');
+  it.todo('should display the total quantity of products');
+  it.todo('should display product (singular) when there is only 1 product');
 });
