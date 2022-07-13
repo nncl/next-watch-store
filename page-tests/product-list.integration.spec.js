efe338a80f6d2964c8e2e5be3cc548025cf5b380
Cauê Almeida <nncl@live.com>
2022-07-12 22:21:45 -0300

    test(components): Testing message bindings
    Such as errors and quantities.

--- page-tests/product-list.integration.spec.js
+++ page-tests/product-list.integration.spec.js
@@ -1,8 +1,10 @@
 import { render, screen, waitFor } from '@testing-library/react';
+import { Response } from 'miragejs';
+
 import ProductList from '../pages/index';
 import { makeServer } from '../miragejs/server';
 
-const renderProduct = () => {
+const renderProductList = () => {
   render(<ProductList />);
 };
 
@@ -18,7 +20,7 @@ describe('ProductList', () => {
   });
 
   it('should render', () => {
-    renderProduct();
+    renderProductList();
 
     expect(screen.getByTestId('product-list')).toBeInTheDocument();
   });
@@ -26,17 +28,37 @@ describe('ProductList', () => {
   it('should render the ProductCard component 10 times', async () => {
     server.createList('product', 10);
 
-    renderProduct();
+    renderProductList();
 
     await waitFor(() => {
       expect(screen.getAllByTestId('product-card')).toHaveLength(10);
     });
   });
 
-  it.todo('should render the no products message');
+  it('should render the no products message', async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('no-products')).toBeInTheDocument();
+    });
+  });
+
+  it('should display error message when promise rejects', async () => {
+    server.get('products', () => {
+      return new Response(500, {}, {});
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('server-error')).toBeInTheDocument();
+      expect(screen.queryByTestId('no-products')).toBeNull();
+      expect(screen.queryAllByTestId('product-card')).toHaveLength(0);
+    });
+  });
+
   it.todo('should render the Search component');
   it.todo('should filter the product list when a search is performed');
-  it.todo('should display error message when promise rejects');
   it.todo('should display the total quantity of products');
   it.todo('should display product (singular) when there is only 1 product');
 });
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,25 @@ import Search from '../components/search';
 import { useFetchProducts } from '../hooks/use-fetch-products';
 
 export default function Home() {
-  const { products } = useFetchProducts();
+  const { products, error } = useFetchProducts();
+
+  const renderProductsOrEmptyMessage = () => {
+    if (!products.length && !error) {
+      return <h4 data-testid="no-products">No products left</h4>;
+    }
+
+    return products.map((product) => (
+      <ProductCard data-testid="product-card" product={product} key={product.id} />
+    ));
+  };
+
+  const renderErrorMessage = () => {
+    if (!error) {
+      return null;
+    }
+
+    return <h4 data-testid="server-error">Error from the server</h4>;
+  };
 
   return (
     <main data-testid="product-list" className="my-8">
@@ -12,9 +30,8 @@ export default function Home() {
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          {products.map((product) => (
-            <ProductCard data-testid="product-card" product={product} key={product.id} />
-          ))}
+          {renderErrorMessage()}
+          {renderProductsOrEmptyMessage()}
         </div>
       </div>
     </main>
