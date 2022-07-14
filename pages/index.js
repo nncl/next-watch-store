99af226d66333ff296e5eb94cb03c03371eb4f9c
Cauê Almeida <nncl@live.com>
2022-07-13 22:30:00 -0300

    feat(pages): Filtering products by search box

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import { useFetchProducts } from '../hooks/use-fetch-products';
+
 import ProductCard from '../components/product-card';
 import Search from '../components/search';
-import { useFetchProducts } from '../hooks/use-fetch-products';
 
 export default function Home() {
   const { products, error } = useFetchProducts();
 
+  const [term, setTerm] = useState('');
+  const [localProducts, setLocalProducts] = useState([]);
+
+  useEffect(() => {
+    if (!term) {
+      setLocalProducts(products);
+    } else {
+      setLocalProducts(
+        products.filter(({ name = '', title = '' }) => {
+          const item = title || name;
+          return item.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        }),
+      );
+    }
+  }, [products, term]);
+
   const renderProductsOrEmptyMessage = () => {
-    if (!products.length && !error) {
+    if (!localProducts.length && !error) {
       return <h4 data-testid="no-products">No products left</h4>;
     }
 
-    return products.map((product) => (
+    return localProducts.map((product) => (
       <ProductCard data-testid="product-card" product={product} key={product.id} />
     ));
   };
@@ -25,7 +44,7 @@ export default function Home() {
 
   return (
     <main data-testid="product-list" className="my-8">
-      <Search />
+      <Search doSearch={(e) => setTerm(e)} />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
