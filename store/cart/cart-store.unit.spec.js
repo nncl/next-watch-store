aa94b1b0e8d980526b6360e84f1dcd80e729d0e2
Cauê Almeida <nncl@live.com>
2022-07-26 22:31:57 -0300

    feat(store): Adding products to state

--- store/cart/cart-store.unit.spec.js
+++ store/cart/cart-store.unit.spec.js
@@ -1,11 +1,44 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import { useCartStore } from './index';
+import { makeServer } from '../../miragejs/server';
 
 describe('Cart Store', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should return open equals false on initial state', () => {
     const { result } = renderHook(useCartStore);
 
     expect(result.current.state.open).toBe(false);
+  });
+
+  it('should have an empty array when store is initialized', () => {
+    const { result } = renderHook(useCartStore);
+
+    expect(Array.isArray(result.current.state.products)).toBe(true);
+    expect(result.current.state.products).toHaveLength(0);
+  });
+
+  it('should have add product in store', () => {
+    const products = server.createList('product', 2);
+
+    const { result } = renderHook(useCartStore);
+    const {
+      actions: { add },
+    } = result.current;
+
+    for (const product of products) {
+      act(() => add(product));
+    }
+
+    expect(result.current.state.products).toHaveLength(2);
   });
 
   it('should toggle open', () => {
