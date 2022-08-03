e0ef56aab4b4dc5d4d5358680ec4d614d6372acd
Cauê Almeida <nncl@live.com>
2022-08-03 20:25:03 -0300

    feat(components): Testing cart

---
+++ components/cart.integration.spec.js
@@ -0,0 +1,75 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { act, render, screen } from '@testing-library/react';
+
+import { makeServer } from '../miragejs/server';
+import { useCartStore } from '../store/cart';
+
+import Cart from './cart';
+
+describe('Cart', () => {
+  let server;
+  let result;
+
+  let add;
+  let toggle;
+  let reset;
+
+  let spy;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+    result = renderHook(() => useCartStore()).result;
+
+    add = result.current.actions.add;
+    toggle = result.current.actions.toggle;
+    reset = result.current.actions.reset;
+
+    spy = jest.spyOn(result.current.actions, 'toggle');
+  });
+
+  afterEach(() => {
+    server.shutdown();
+    jest.clearAllMocks();
+  });
+
+  it('should have class "hidden" in the component', () => {
+    render(<Cart />);
+
+    expect(screen.getByTestId('cart')).toHaveClass('hidden');
+  });
+
+  it('should NOT have class "hidden" in the component', () => {
+    render(<Cart />);
+
+    act(() => toggle());
+
+    expect(screen.getByTestId('cart')).not.toHaveClass('hidden');
+  });
+
+  it('should call store toggle() twice', () => {
+    render(<Cart />);
+
+    const button = screen.getByTestId('close-button');
+
+    act(() => {
+      button.click();
+      button.click();
+    });
+
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+
+  it('should display 2 product cards', () => {
+    const products = server.createList('product', 2);
+
+    act(() => {
+      for (const product of products) {
+        add(product);
+      }
+    });
+
+    render(<Cart />);
+
+    expect(screen.getAllByTestId('cart-item')).toHaveLength(2);
+  });
+});
--- components/cart.js
+++ components/cart.js
@@ -7,13 +7,18 @@ export default function Cart() {
 
   return (
     <div
+      data-testid="cart"
       className={`${
         !open ? 'hidden' : ''
       } fixed right-0 top-0 max-w-xs w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white border-l-2 border-gray-300`}
     >
       <div className="flex items-center justify-between">
         <h3 className="text-2xl font-medium text-gray-700">Your cart</h3>
-        <button onClick={() => toggle()} className="text-gray-600 focus:outline-none">
+        <button
+          data-testid="close-button"
+          onClick={() => toggle()}
+          className="text-gray-600 focus:outline-none"
+        >
           <svg
             className="h-5 w-5"
             fill="none"
